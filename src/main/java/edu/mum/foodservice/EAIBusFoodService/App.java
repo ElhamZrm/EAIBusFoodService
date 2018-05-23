f9f3d6039af90d607aa7ede025ee1a96ed965302
ElhamZrm <ezarrinmehr@mum.edu>
2018-05-23 11:44:36 -0500

    first commit

---
+++ src/main/java/edu/mum/foodservice/EAIBusFoodService/App.java
@@ -0,0 +1,13 @@
+package edu.mum.foodservice.EAIBusFoodService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
